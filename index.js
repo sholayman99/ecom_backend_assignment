0eee39f965a94405f20d22dd09ea17a2f9984dac
Md Sholayman <mdsholayman998@gmail.com>
2023-12-23 22:25:19 +0600

    initialize the project

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+* Author: Md. Sholayman
+* Description: This is the file where the app is running.
+* Date : 23 December 2023
+* */
+
+const app = require("./app");
+
+app.listen(``)
---
+++ src/routes/api.js
@@ -0,0 +1,13 @@
+/*
+* Author: Md. Sholayman
+* Description: This is the file that contains all the api end-point of the app
+* Date : 23 December 2023
+* */
+
+const express = require("express");
+const router = express.Router();
+
+
+
+
+module.exports = router;
